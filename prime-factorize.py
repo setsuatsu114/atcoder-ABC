b0132fb3c88a66df914c0ed1398fd19603814a21
setsuatsu114 <118243126+setsuatsu114@users.noreply.github.com>
2023-12-30 04:15:58 +0900

    third commit

---
+++ prime-factorize.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+def prime_factorize(n):
+    f = 2
+    a = []
+    while n%f==0:
+        a.append(f)
+        n //= f
+    print(a)
+    i = 3
+    while True:
+        if n%i==0:
+            a.append(i)
+            n //= i
+        else:
+            i += 1
+        if n==1:
+            break
+    return a
+        
+print(prime_factorize(N))
